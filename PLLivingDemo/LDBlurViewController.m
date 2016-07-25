4b1c7c70a009f90e3cd1febc96e5c25fb9833c3d
Moskize91 <xiangtantaozeyu@gmail.com>
2016-07-25 15:42:30 +0800

    refactor: methods can be called only once.

--- PLLivingDemo/LDBlurViewController.m
+++ PLLivingDemo/LDBlurViewController.m
@@ -11,6 +11,8 @@
 @implementation LDBlurViewController
 {
     UIVisualEffectView *_blurBackgroundView;
+    BOOL _didPlayedAppearAnimation;
+    BOOL _didPlayedDisappearAnimation;
 }
 
 - (instancetype)initWithPresentOrientation:(LDBlurViewControllerPresentOrientation)presentOrientation
@@ -32,44 +34,52 @@
 
 - (void)playAppearAnimationWithComplete:(void (^)())complete
 {
-    [self.view.superview insertSubview:_blurBackgroundView belowSubview:self.view];
-    [_blurBackgroundView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.bottom.left.and.right.equalTo(self.view.superview);
-    }];
-    CGRect frame = self.view.superview.bounds;
-    _blurBackgroundView.frame = frame;
-    self.view.hidden = NO;
-    
-    [self _resetOrigin:&frame.origin withPresentOrientation:self.presentOrientation];
-    self.view.frame = frame;
-    
-    [UIView animateWithDuration:0.55 animations:^{
-        _blurBackgroundView.effect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
-        self.view.frame = self.view.superview.bounds;
+    if (!_didPlayedAppearAnimation) {
         
-    } completion:^(BOOL finished) {
-        if (complete) {
-            complete();
-        }
-    }];
+        [self.view.superview insertSubview:_blurBackgroundView belowSubview:self.view];
+        [_blurBackgroundView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.bottom.left.and.right.equalTo(self.view.superview);
+        }];
+        CGRect frame = self.view.superview.bounds;
+        _blurBackgroundView.frame = frame;
+        self.view.hidden = NO;
+        
+        [self _resetOrigin:&frame.origin withPresentOrientation:self.presentOrientation];
+        self.view.frame = frame;
+        
+        [UIView animateWithDuration:0.55 animations:^{
+            _blurBackgroundView.effect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
+            self.view.frame = self.view.superview.bounds;
+            
+        } completion:^(BOOL finished) {
+            if (complete) {
+                complete();
+            }
+        }];
+        _didPlayedAppearAnimation = YES;
+    }
 }
 
 - (void)playDisappearAnimationWithComplete:(void (^)())complete
 {
-    [_blurBackgroundView addSubview:self.view];
-    
-    CGRect frame = self.view.frame;
-    [self _resetOrigin:&frame.origin withPresentOrientation:self.presentOrientation];
-    
-    [UIView animateWithDuration:0.55 animations:^{
-        _blurBackgroundView.effect = nil;
-        self.view.frame = frame;
-    } completion:^(BOOL finished) {
-        [_blurBackgroundView removeFromSuperview];
-        if (complete) {
-            complete();
-        }
-    }];
+    if (!_didPlayedDisappearAnimation) {
+        
+        [_blurBackgroundView addSubview:self.view];
+        
+        CGRect frame = self.view.frame;
+        [self _resetOrigin:&frame.origin withPresentOrientation:self.presentOrientation];
+        
+        [UIView animateWithDuration:0.55 animations:^{
+            _blurBackgroundView.effect = nil;
+            self.view.frame = frame;
+        } completion:^(BOOL finished) {
+            [_blurBackgroundView removeFromSuperview];
+            if (complete) {
+                complete();
+            }
+        }];
+        _didPlayedDisappearAnimation = YES;
+    }
 }
 
 - (void)_resetOrigin:(CGPoint *)pOrigin withPresentOrientation:(LDBlurViewControllerPresentOrientation)orinetation
