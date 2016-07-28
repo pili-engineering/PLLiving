246df785c136ca10def40d68ee2b679ee257f5cc
Moskize91 <xiangtantaozeyu@gmail.com>
2016-07-28 11:24:38 +0800

    modify login view controller

--- PLLivingDemo/LDLoginViewController.m
+++ PLLivingDemo/LDLoginViewController.m
@@ -8,6 +8,7 @@
 
 #import "LDLoginViewController.h"
 #import "LDLobbyViewController.h"
+#import "LDAppearanceView.h"
 
 @interface LDLoginViewController ()
 
@@ -21,13 +22,71 @@
 {
     [super viewDidLoad];
     
-    _loginButton = ({
+    ({
+        UIView *backgroundView = [[LDAppearanceView alloc] initWithLayer:({
+            CAGradientLayer *gradientLayer = [CAGradientLayer layer];
+            gradientLayer.startPoint = CGPointMake(0, 0);
+            gradientLayer.endPoint = CGPointMake(0, 1);
+            gradientLayer.locations = @[@0, @0.33, @0.66, @1];
+            gradientLayer.colors = @[(__bridge id) [UIColor colorWithHexString:@"1A000000"].CGColor,
+                                     (__bridge id) [UIColor colorWithHexString:@"005A5A5A"].CGColor,
+                                     (__bridge id) [UIColor colorWithHexString:@"00282828"].CGColor,
+                                     (__bridge id) [UIColor colorWithHexString:@"AB000000"].CGColor,];
+            gradientLayer;
+        })];
+        [self.view addSubview:backgroundView];
+        [backgroundView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.bottom.left.and.right.equalTo(self.view);
+        }];
+    });
+    
+    ({
+        UIImageView *logImageView = [[UIImageView alloc] init];
+        logImageView.image = [UIImage imageNamed:@"logo"];
+        [self.view addSubview:logImageView];
+        [logImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(self.view).with.offset(170);
+            make.centerX.equalTo(self.view);
+        }];
+    });
+    
+    ({
+        UIImageView *titleImageView = [[UIImageView alloc] init];
+        titleImageView.image = [UIImage imageNamed:@"LIVING"];
+        [self.view addSubview:titleImageView];
+        [titleImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(self.view).with.offset(335);
+            make.centerX.equalTo(self.view);
+        }];
+    });
+    
+    ({
+        UILabel *sloganLabel = [[UILabel alloc] init];
+        [self.view addSubview:sloganLabel];
+        sloganLabel.text = LDString("slogan");
+        sloganLabel.textColor = [UIColor colorWithHexString:@"4E4E4E"];
+        sloganLabel.font = [UIFont systemFontOfSize:16];
+        [sloganLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(self.view).with.offset(398);
+            make.centerX.equalTo(self.view);
+        }];
+    });
+    
+    self.loginButton = ({
         UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
-        [button setTitle:LDString("login-by-wechat") forState:UIControlStateNormal];
         [self.view addSubview:button];
+        [button setTitle:LDString("login-by-phone-number") forState:UIControlStateNormal];
+        [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [button.titleLabel setFont:[UIFont systemFontOfSize:14]];
+        button.layer.cornerRadius = 25;
+        button.layer.borderWidth = 1;
+        button.layer.borderColor = [UIColor blackColor].CGColor;
+        
         [button addTarget:self action:@selector(_onPressedLoginButton:) forControlEvents:UIControlEventTouchUpInside];
         [button mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.center.equalTo(self.view);
+            make.centerX.equalTo(self.view);
+            make.top.equalTo(self.view).with.offset(504);
+            make.size.mas_equalTo(CGSizeMake(220, 50));
         }];
         button;
     });
