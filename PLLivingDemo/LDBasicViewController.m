69a7e3af9de9df2a102bf290da21bb9db76d99df
Moskize91 <xiangtantaozeyu@gmail.com>
2016-08-22 14:33:03 +0800

    reset view callback.

--- PLLivingDemo/LDBasicViewController.m
+++ PLLivingDemo/LDBasicViewController.m
@@ -60,18 +60,12 @@
                  completion:(void (^ __nullable)(void))completion
 {
     if (![self.viewControllers containsObject:viewController]) {
-        
-        UIViewController *originalViewController = self.topViewController;
-        
         [self.viewControllers addObject:viewController];
         UIView *view = viewController.view;
         view.autoresizingMask = UIViewAutoresizingFlexibleWidth |
                                 UIViewAutoresizingFlexibleHeight;
         [self.view addSubview:view];
         CGRect targetFrame = self.view.bounds;
-        
-        [originalViewController viewWillDisappear:animatedFlag];
-        [viewController viewWillAppear:animatedFlag];
         
         if (animatedFlag) {
             _maskAllTouchEvents = YES;
@@ -88,16 +82,12 @@
                 if (completion) {
                     completion();
                 }
-                [originalViewController viewDidDisappear:animatedFlag];
-                [viewController viewDidAppear:animatedFlag];
             }];
         } else {
             view.frame = targetFrame;
             if (completion) {
                 completion();
             }
-            [originalViewController viewDidDisappear:animatedFlag];
-            [viewController viewDidAppear:animatedFlag];
         }
         [self setNeedsStatusBarAppearanceUpdate];
     }
@@ -115,17 +105,8 @@
                   completion:(void (^ __nullable)(void))completion
 {
     if ([self.viewControllers containsObject:viewController]) {
-        
-        UIViewController *originalTopViewController = self.topViewController;
         [self.viewControllers removeObject:viewController];
-        UIViewController *newTopViewController = self.topViewController;
-        
         UIView *view = viewController.view;
-        
-        if (originalTopViewController == viewController) {
-            [viewController viewWillDisappear:animatedFlag];
-            [newTopViewController viewWillAppear:animatedFlag];
-        }
         
         if (animatedFlag) {
             _maskAllTouchEvents = YES;
@@ -140,20 +121,11 @@
                 if (completion) {
                     completion();
                 }
-                
-                if (originalTopViewController == viewController) {
-                    [viewController viewDidDisappear:animatedFlag];
-                    [newTopViewController viewDidAppear:animatedFlag];
-                }
             }];
         } else {
             [view removeFromSuperview];
             if (completion) {
                 completion();
-            }
-            if (originalTopViewController == viewController) {
-                [viewController viewDidDisappear:animatedFlag];
-                [newTopViewController viewDidAppear:animatedFlag];
             }
         }
         [self setNeedsStatusBarAppearanceUpdate];
