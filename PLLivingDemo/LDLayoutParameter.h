1435eaef578d6bd571538a852d83f21336b22776
Moskize91 <xiangtantaozeyu@gmail.com>
2016-08-23 10:18:03 +0800

    remove useless file.

--- PLLivingDemo/LDLayoutParameter.h
+++
@@ -1,9 +0,0 @@
-//
-//  LDLayoutParameter.h
-//  PLLivingDemo
-//
-//  Created by TaoZeyu on 16/7/18.
-//  Copyright © 2016年 com.pili-engineering. All rights reserved.
-//
-
-#define klayStatusBarHeight 20
